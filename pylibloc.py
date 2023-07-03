cabb560c00a84ff331a5bddca1a5feb203bdc304
gereoffy <arpi.git@interoot.hu>
2023-07-03 08:20:23 +0000

    2x speed optim! (with pypy it's even 2x faster than original libloc)

--- pylibloc.py
+++ pylibloc.py
@@ -53,6 +53,17 @@ class LocDB:
     self.v4root=nxt
     if debug: print("IPv4 root node:",nxt)
 
+    # map cc codes to dict for faster lookup:   (only 254 codes!)
+    self.cc_dict={}
+    pos=0
+    while pos<len(self.data["co"]):
+        key=self.data["co"][pos:pos+2]
+        cont=self.data["co"][pos+2:pos+4]      # continent code (2 ascii chars)
+        nid=int.from_bytes(self.data["co"][pos+4:pos+8],byteorder="big",signed=False)
+        self.cc_dict[key]=( key.decode(), cont.decode(), self.getstr(nid) )
+        pos+=8
+    if debug: print("Country-codes found:",len(self.cc_dict))
+
     if debug>1:
         pos=0
         maxnet=0
@@ -80,7 +91,7 @@ class LocDB:
   def get_as(self, asfind):
     p1,p2=0,len(self.data["as"])//8
     while p1<p2:
-        pos=(p1+p2)//2  # do binary search!
+        pos=(p1+p2)//2  # do binary search!  avg 15 steps/lookup
         x=int.from_bytes(self.data["as"][pos*8:pos*8+4],byteorder="big",signed=False)
         if asfind==x:
             nid=int.from_bytes(self.data["as"][pos*8+4:pos*8+8],byteorder="big",signed=False)
@@ -89,17 +100,8 @@ class LocDB:
         else: p2=pos
     return "N/A"
 
-  def get_cc(self, ccfind):
-    pos=0
-    while pos<len(self.data["co"]):
-        if ccfind==self.data["co"][pos:pos+2]:     # (code+continent+name)
-            cont=self.data["co"][pos+2:pos+4]      # continent code (2 ascii chars)
-            nid=int.from_bytes(self.data["co"][pos+4:pos+8],byteorder="big",signed=False)
-            return ccfind.decode(),cont.decode(),self.getstr(nid)
-        pos+=8
-    return None
-
-  def lookuptree(self,address,nxt=0,debug=False):
+  def lookuptree(self,address,map4=False,debug=False):
+    nxt=self.v4root if map4 else 0
     ret=(-1,0)
     mask=0
     while mask<len(address)*8:
@@ -116,15 +118,34 @@ class LocDB:
       mask+=1
     return ret
 
+  # IPv4-optimized version of generic lookuptree,  len(address) must be 4 bytes (32 bits)
+  def lookuptree4(self,address):
+    data=self.data["nt"]
+    nxt=12*self.v4root
+    ret=(-1,0)
+    ip=int.from_bytes(address,byteorder="big",signed=False)
+    mask=0
+    while mask<32:
+      bit=(ip>>29)&4  # == 4*((ip>>31)&1)
+#      net=(data[nxt+8]<<24)|(data[nxt+9]<<16)|(data[nxt+10]<<8)|data[nxt+11]
+      net=int.from_bytes(data[nxt+8:nxt+12],byteorder="big",signed=True)
+      if net>=0: ret=(net,mask)
+      nxt=12*int.from_bytes(data[nxt+bit:nxt+bit+4],byteorder="big",signed=False)
+      if nxt==0: break
+      mask+=1
+      ip<<=1
+    return ret
+
   def lookup6(self, address, map4=False):
-    pos,mask=self.lookuptree(address,self.v4root if map4 else 0)
+    pos,mask=self.lookuptree4(address) if map4 else self.lookuptree(address)
+#    pos,mask=self.lookuptree(address,map4)
     if pos<0: return # not found
     node=self.data["nd"][pos*12:pos*12+12] # network data: countrycode(2) + padding(2) + ASN(4) + flags(2) +padding(2)
     co=node[0:2] # country code
     asn=int.from_bytes(node[4:8],byteorder="big",signed=False)
     flags=int.from_bytes(node[8:10],byteorder="big",signed=False)
     ass=self.get_as(asn) # AS: string from number
-    cos=self.get_cc(co)  # Country code, continent code & country name
+    cos=self.cc_dict.get(co,None)  # Country code, continent code & country name
     # print(pos,node[0:2],asn,ass,node.hex(' '))
     return cos,asn,ass,flags,mask
 
@@ -145,7 +166,26 @@ if __name__ == "__main__":
     for ip in sys.argv[1:]: print(db.lookup(ip))
   else:
     print(db.lookup4(bytes([193,224,41,22])))
+#    exit(0)
     print(db.lookup6(bytes([0x2a,1,0x6e,0xe0, 0,1, 2,1,   0,0,0,0,0xB,0xAD,0xC0,0xDE])))
     print(db.lookup("1.1.1.1"))
     print(db.lookup("2a00:1450:400d:806::2005"))
+    
+    import time
+    t0=time.time()
+    cimek=[]
+    from ipaddress import ip_address
+    for line in open("v46cimek2","rt"):
+        address=ip_address(line.strip()).packed
+#        if len(address)==16:
+        cimek.append(address) # string -> bytes
+    print("Load time: %d ms"%(1000.0*(time.time()-t0)))
+
+#    t0=time.time()
+    for address in cimek:
+#        pos,mask=db.lookuptree(address,db.v4root if len(address)==4 else 0)
+        res=db.lookup6(address, len(address)==4)
+#        if not res: print("%d.%d.%d.%d"%(address[0],address[1],address[2],address[3]))
+    t0=time.time()-t0
+    print("Lookup time: %d ms  avg: %5.3f ns/ip (%d total)"%(1000.0*t0, 1000000.0*t0/len(cimek), len(cimek) ))
 
